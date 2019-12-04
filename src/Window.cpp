6c23ace6001df331fa5ee300d375ec69c667fe3d
Emilien Lemaire <emilienlemaire@Emiliens-MacBook-Air.local>
2019-12-04 18:39:08 +0100

    Added escape key

--- src/Window.cpp
+++ src/Window.cpp
@@ -51,7 +51,7 @@ void Window::show() {
 
         glfwSwapBuffers(m_Window);
         glfwPollEvents();
-    } while(!glfwWindowShouldClose(m_Window) && m_EscToQuit ? glfwGetKey(m_Window, GLFW_KEY_ESCAPE) != GLFW_PRESS : true);
+    } while(!glfwWindowShouldClose(m_Window) && m_EscToQuit ? glfwGetKey(m_Window, GLFW_KEY_ESCAPE) != GLFW_PRESS : false);
 
     Log::Info("Window closed");
 }
